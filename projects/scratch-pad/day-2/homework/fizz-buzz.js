bd32dd812941d67abbf886bce6c1a743cf06427f
Jeff Watkins <jeff.l.watkins2@gmail.com>
2018-10-24 01:46:13 +0000

    loops.js

---
+++ projects/scratch-pad/day-2/homework/fizz-buzz.js
@@ -0,0 +1,39 @@
+// #!/usr/bin/env node
+
+'use strict';
+
+/**
+ * 2: Fizz Buzz
+ *
+ *  a. Write a Function called fizzBuzz that prints the numbers from 1 to 100.
+ *  b. BUT, for multiples of three print “Fizz” instead of the number.
+ *  c. AND for the multiples of five print “Buzz”.
+ *  d. FINALLY, for numbers which are multiples of both three and five print “FizzBuzz”.
+*/
+function fizzBuzz() {
+    // YOUR CODE GOES BELOW HERE //
+for (var number = 1; number <= 100; number += 1) {
+  if (number % 15 == 0) {
+    console.log("FizzBuzz");
+  } else if (number % 3 == 0) {
+    console.log("Fizz");
+  } else if (number % 5 == 0) {
+    console.log("Buzz");
+  } else {
+  console.log(number); }
+}
+
+
+    // YOUR CODE GOES ABOVE HERE //
+}
+
+
+
+
+
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
+if((typeof process !== 'undefined') &&
+   (typeof process.versions.node !== 'undefined')) {
+    // here, export any references you need for tests //
+    module.exports.fizzBuzz = fizzBuzz
+}
